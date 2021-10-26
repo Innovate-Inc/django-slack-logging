b080cc0b830276cf1924fc7af3515324d96405f4
Travis Bock <tbock@innovateteam.com>
2021-10-26 10:59:28 -0700

    add optional bot_username

--- slack_logging/__init__.py
+++ slack_logging/__init__.py
@@ -3,9 +3,11 @@ from logging import Handler
 
 
 class SlackExceptionHandler(Handler):
-    def __init__(self, bot_token, channel_id):
+    def __init__(self, bot_token, channel_id, bot_username=None):
         self.bot_token = bot_token
         self.channel_id = channel_id
+        if bot_username is not None:
+            self.bot_username = bot_username
         super(SlackExceptionHandler, self).__init__()
 
     def emit(self, record):
@@ -16,8 +18,12 @@ class SlackExceptionHandler(Handler):
                 user = 'Unknown'
 
             msg = self.format(record)
+            data = {"text": f"{record.levelname.title()} experienced by {user}", "channel": self.channel_id}
+            if self.bot_username is not None:
+                data['username'] = self.bot_username
+
             requests.post("https://slack.com/api/chat.postMessage", headers={"Authorization": f"Bearer {self.bot_token}"},
-                          data={"text": f"{record.levelname.title()} experienced by {user}", "channel": self.channel_id})
+                          data=data)
             requests.post("https://slack.com/api/files.upload", headers={"Authorization": f"Bearer {self.bot_token}"},
                           data={"content": msg, "channels": self.channel_id, "filetype": "python", "filename": "traceback.py"})
 
