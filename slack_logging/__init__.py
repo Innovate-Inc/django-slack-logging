c61b9e15643629e4162553b105c65307fabe774c
Travis Bock <tbock@innovateteam.com>
2021-10-26 11:10:11 -0700

    remove bot username for now

--- slack_logging/__init__.py
+++ slack_logging/__init__.py
@@ -3,11 +3,9 @@ from logging import Handler
 
 
 class SlackExceptionHandler(Handler):
-    def __init__(self, bot_token, channel_id, bot_username=None):
+    def __init__(self, bot_token, channel_id):
         self.bot_token = bot_token
         self.channel_id = channel_id
-        if bot_username is not None:
-            self.bot_username = bot_username
         super(SlackExceptionHandler, self).__init__()
 
     def emit(self, record):
@@ -18,12 +16,8 @@ class SlackExceptionHandler(Handler):
                 user = 'Unknown'
 
             msg = self.format(record)
-            data = {"text": f"{record.levelname.title()} experienced by {user}", "channel": self.channel_id}
-            if self.bot_username is not None:
-                data['username'] = self.bot_username
-
             requests.post("https://slack.com/api/chat.postMessage", headers={"Authorization": f"Bearer {self.bot_token}"},
-                          data=data)
+                          data={"text": f"{record.levelname.title()} experienced by {user}", "channel": self.channel_id})
             requests.post("https://slack.com/api/files.upload", headers={"Authorization": f"Bearer {self.bot_token}"},
                           data={"content": msg, "channels": self.channel_id, "filetype": "python", "filename": "traceback.py"})
 
