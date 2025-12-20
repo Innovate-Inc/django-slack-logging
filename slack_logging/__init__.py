2cdae2ac0c0ba6445556390c61b41ae95ddb880c
Travis <tbock@innovateteam.com>
2025-12-19 16:21:21 -0800

    dislike case sensitive urls

--- slack_logging/__init__.py
+++ slack_logging/__init__.py
@@ -25,7 +25,7 @@ class SlackExceptionHandler(Handler):
             msg = self.format(record)
             requests.post("https://slack.com/api/chat.postMessage", headers={"Authorization": f"Bearer {self.bot_token}"},
                           data={"text": f"{record.levelname.title()} experienced by {user}", "channel": self.channel_id})
-            r = requests.post("https://slack.com/api/files.getUploadUrlExternal", headers={"Authorization": f"Bearer {self.bot_token}"},
+            r = requests.post("https://slack.com/api/files.getUploadURLExternal", headers={"Authorization": f"Bearer {self.bot_token}"},
                              data={"filename": "traceback.py", "snippet_type": "python", "length": len(msg), })
             upload_url = r.json()["upload_url"]
             file_id = r.json()["file_id"]
